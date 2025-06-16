aabf2de0bc830e80bc229ae7c67fc9220d55118e
Konoplenko <canabiis88@gmail.com>
2025-06-16 23:14:43 +0300

    breakdown by vue

--- skypro-kanban/src/views/HomeView.vue
+++ skypro-kanban/src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <ExitModal />
+    <NewCardModal />
+    <TaskModal />
+
+    <BaseHeader />
+    <main class="main">
+      <div class="container">
+        <TaskDesk />
+      </div>
+    </main>
+  </div>
 </template>
+
+<script>
+import BaseHeader from '@/components/BaseHeader.vue';
+import TaskDesk from '@/components/TaskDesk.vue';
+import ExitModal from '@/components/ExitModal.vue';
+import NewCardModal from '@/components/NewCardModal.vue';
+import TaskModal from '@/components/TaskModal.vue';
+
+export default {
+  components: {
+    BaseHeader,
+    TaskDesk,
+    ExitModal,
+    NewCardModal,
+    TaskModal
+  }
+}
+</script>
