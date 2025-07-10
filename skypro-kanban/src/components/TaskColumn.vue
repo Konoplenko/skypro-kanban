8d6342996414f3657b67a5f2fdd6bf5634a1c1e3
Konoplenko <canabiis88@gmail.com>
2025-07-10 11:42:22 +0300

    style TaskColumn

--- skypro-kanban/src/components/TaskColumn.vue
+++ skypro-kanban/src/components/TaskColumn.vue
@@ -16,33 +16,77 @@
 </template>
 
 <script>
-import TaskCard from './TaskCard.vue';
+import TaskCard from './TaskCard.vue'
 
 export default {
   components: {
-    TaskCard
+    TaskCard,
   },
   props: {
     status: String,
-    tasks: Array
+    tasks: Array,
   },
   methods: {
     getThemeClass(topic) {
       const themeMap = {
         'Web Design': 'orange',
-        'Research': 'green',
-        'Copywriting': 'purple',
-      };
-      return themeMap[topic] || 'gray';
+        Research: 'green',
+        Copywriting: 'purple',
+      }
+      return themeMap[topic] || 'gray'
     },
     formatDate(dateString) {
-      const date = new Date(dateString);
-      return date.toLocaleDateString('ru-RU', {
-        day: '2-digit',
-        month: '2-digit',
-        year: '2-digit'
-      }).replace(/\//g, '.');
-    }
-  }
-};
+      const date = new Date(dateString)
+      return date
+        .toLocaleDateString('ru-RU', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit',
+        })
+        .replace(/\//g, '.')
+    },
+  },
+}
 </script>
+
+<style scoped>
+.main__column {
+  width: 20%;
+  margin: 0 auto;
+  display: block;
+}
+
+.column__title {
+  padding: 0 10px;
+  margin: 15px 0;
+}
+
+.column__title p {
+  color: #94a6be;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+  text-transform: uppercase;
+  margin: 15px 0;
+}
+
+.cards {
+  width: 100%;
+  display: block;
+  position: relative;
+}
+
+@media screen and (max-width: 1200px) {
+  .main__column {
+    width: 100%;
+    margin: 0 auto;
+    display: block;
+  }
+
+  .cards {
+    width: 100%;
+    display: flex;
+    overflow-y: auto;
+  }
+}
+</style>
